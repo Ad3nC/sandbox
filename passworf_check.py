4a8b8c67ed81413012be0075fb259d6c59c4c796
Aden <aden.comas@my.jcu.edu.au>
2019-08-21 11:39:22 +1000

    added password checking program

--- passworf_check.py
+++ passworf_check.py
@@ -1,5 +1,8 @@
 def main():
-    print("hello world ")
-    
+    password = input("Enter a password")
+    while len(password) < 5:
+        password = input("Enter a password")
+    print("*" * len(password))
+
 
 main()
